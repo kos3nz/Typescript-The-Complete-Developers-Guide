1388bbe3561030d511d2a8cad6e6de555c5cbd8d
KosM <kosukemorimoto1@gmail.com>
2021-11-04 17:26:02 +0900

    turn CsvFileReader into abstract and generics, and created MatchReader class just for football.csv

--- Stats/src/CsvFileReader.ts
+++ Stats/src/CsvFileReader.ts
@@ -1,33 +1,19 @@
 import fs from 'fs';
-import path from 'path';
-import { dateStringToDate } from './utils';
-import { MatchResult } from './MatchResult';
 
-// Tuple Type
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class FootballCsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
 
+  abstract mapRow(row: string[]): T;
+
   read(): void {
     this.data = fs
-      .readFileSync(path.resolve(__dirname, this.filename), {
+      .readFileSync(this.filename, {
         encoding: 'utf-8',
       })
       .split('\n')
       .map((row: string): string[] => row.split(','))
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult, // 'H' | 'A' | 'D'
-          row[6],
-        ];
-      });
+      .map(this.mapRow);
   }
 }
--- Stats/src/utils.ts
+++ Stats/src/utils.ts
@@ -1,10 +1,8 @@
 export const dateStringToDate = (dateString: string) => {
-  // '11/2/2021' → [11, 2, 2021]
+  // ex. '11/2/2021' → [11, 2, 2021]
   const dateParts = dateString
     .split('/')
     .map((value: string): number => parseInt(value));
 
   return new Date(dateParts[2], dateParts[1] - 1, dateParts[0]);
 };
-
-console.log(dateStringToDate('11/2/2021'));
