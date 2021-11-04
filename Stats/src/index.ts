00d782e3d69b32f65f00ee0dde3176a9c078a4ca
KosM <kosukemorimoto1@gmail.com>
2021-11-04 18:59:18 +0900

    refactored CsvFileReader and MatchReader class with another design pattern called Composition

--- Stats/src/index.ts
+++ Stats/src/index.ts
@@ -1,12 +1,20 @@
-import { MatchReader } from './MatchReader';
+import { CsvFileReader } from './composition/CsvFileReader';
+import { MatchReader } from './composition/MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+// NOTE: Favor object composition over class inheritance, and do not misunderstand the concept!!
+
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUtdWins = 0;
 
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin)
     manUtdWins++;
   if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin)
