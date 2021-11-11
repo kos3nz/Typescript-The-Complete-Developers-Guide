d252dc44a2bca9b36e270274146dbe1febdab0ac
KosM <kosukemorimoto1@gmail.com>
2021-11-11 16:59:48 +0900

    added more decorator examples

--- Features/decorators.ts
+++ Features/decorators.ts
@@ -1,28 +1,55 @@
+@classDecorator
 class Boat {
+  // @testDecorator
   color: string = 'red';
 
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
-  @testDecorator
-  pilot(): void {
+  @logError('Oops, boat was sunk in ocean')
+  pilot(
+    @parameterDecorator speed: string,
+    @parameterDecorator generateWake: boolean
+  ): void {
+    throw new Error();
     console.log('swish');
   }
 }
 
-// First argument is the prototype of the object
-// Second argument is the key of the property/method/accessor on the object
-// Third argument is the property descriptor
-// Decorators are applied when the code for this class is ran (not when an instance is created)
+// Decorators are being executed before an instance is created
 function testDecorator(
   target: any,
   key: string,
   desc: PropertyDescriptor
 ): void {
-  console.log('Target:', target); // Target: Boat { pilot: [Function (anonymous)]}
-  console.log('Key:', key); // Key: pilot
+  // console.log(desc.value); // Cannot read properties of undefined
 }
 
-// in this code, @testDecorator is just doing this ↓
-// testDecorator(Boat.prototype, 'pilot')
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index); // pilot 1, pilot 0
+}
+
+function classDecorator(constructor: typeof Boat) {
+  console.log(constructor); // [Function: Boat]
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+    // desc.value = {
+    // throw new Error();
+    // console.log('swish');
+    // }
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    };
+  };
+}
+
+// new Boat().pilot();
